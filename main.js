a7d17486a88ef6010f286edea0bb6040898e6edf
SamL0823 <36527060+SamL0823@users.noreply.github.com>
2018-09-09 11:26:37 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,85 +1,32 @@
 import 'ol/ol.css';
-import GeoJSON from 'ol/format/GeoJSON';
 import Map from 'ol/Map';
-import VectorLayer from 'ol/layer/Vector';
-import VectorSource from 'ol/source/Vector';
 import View from 'ol/View';
-import sync from 'ol-hashed';
-import DragAndDrop from 'ol/interaction/DragAndDrop';
-import Modify from 'ol/interaction/Modify';
-import Draw from 'ol/interaction/Draw';
-import Snap from 'ol/interaction/Snap';
-import {Fill, Stroke, Style} from 'ol/style';
-import {getArea} from 'ol/sphere';
-import colormap from 'colormap';
+import TileLayer from 'ol/layer/Tile';
+import XYZSource from 'ol/source/XYZ';
+import {fromLonLat} from 'ol/proj';
 
-const source = new VectorSource();
+const key = 'pk.eyJ1IjoiZW55bzA4MjMiLCJhIjoiY2psdjF3OWpqMDJoaTN2cGdqY2o2YmFmOSJ9.XZ6SAz6HUPTqYdUnmOStbA';
 
-const map = new Map({
-  target: 'map-container',
-  
-  view: new View({
-    center: [0, 0],
-    zoom: 2
-  })
+const elevation = new XYZSource({
+    url: 'https://api.mapbox.com/v4/mapbox.terrain-rgb/{z}/{x}/{y}.pngraw?access_token=' + key,
+    crossOrigin: 'anonymous'
 });
-sync(map);
-const layer = new VectorLayer({
-    source: source,
-    style: function(feature) {
-        return new Style({
-        fill: new Fill({
-            color: getColor(feature)
+
+new Map({
+    target: 'map-container',
+    layers: [
+        new TileLayer({
+            opacity: 0.8,
+            source: elevation
         }),
-        stroke: new Stroke({
-            color: 'rgba(255,255,255,0.8)'
+        new TileLayer({
+            source: new XYZSource({
+                url: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg'
+            })
         })
-  });
-}
+    ],
+    view: new View({
+        center: fromLonLat([-71.06, 42.37]),
+        zoom: 12
+    })
 });
-  map.addLayer(layer);
-  map.addInteraction(new DragAndDrop({ 
-      source: source, 
-      formatConstructors: [GeoJSON] 
-})); 
-map.addInteraction(new Modify({
-    source: source 
-}));
-map.addInteraction(new Draw({
-    type: 'Polygon',
-    source: source 
-}));
-map.addInteraction(new Snap({
-    source: source 
-}));
-const clear = document.getElementById('clear');
-clear.addEventListener('click', function() {
-    source.clear();    
-});
-const format = new GeoJSON({featureProjection: 'EPSG:3857'});
-const download = document.getElementById('download');
-source.on('change', function() {
-    const features = source.getFeatures();
-    const json = format.writeFeatures(features);
-    download.href = 'data:text/json;charset=utf-8,' + json;
-});
-
-const min = 1e8; // the smallest area
-const max = 2e13; // the biggest area
-const steps = 50;
-const ramp = colormap({
-    colormap: 'blackbody',
-    nshades: steps
-});
-
-function clamp(value, low, high) {
-    return Math.max(low, Math.min(value, high));
-}
-
-function getColor(feature) {
-    const area = getArea(feature.getGeometry());
-    const f = Math.pow(clamp((area - min) / (max - min), 0, 1), 1 / 2);
-    const index = Math.round(f * (steps - 1));
-    return ramp[index];
-}
-
